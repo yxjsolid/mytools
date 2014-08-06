f07d675a3120bbd988894e125ac5d6f0e3b2055d
yxjsolid <yxjsolid@gmail.com>
2014-08-06 17:14:06 +0800

    update

--- dhcp/genDhcpInform.py
+++ dhcp/genDhcpInform.py
@@ -2,10 +2,10 @@ from dhcpUtil import *
 
 
 if __name__ == "__main__":
-    clientMac = "00:17:22:33:44:55"
-    clientIp = "192.168.1.1"
-    #dstIp = "255.255.255.255"
-    dstIp = "192.168.168.2"
+    clientMac = "78:2b:cb:c9:e8:41"
+    clientIp = "3.3.3.5"
+    dstIp = "255.255.255.255"
+    #dstIp = "192.168.168.2"
 
     reqList = [1,15,3,6,44,46,47,31,33,121,249,43,244]
     isBroadcast = 1
